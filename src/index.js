5a3cff55c7258cfc471a352b5013cc3365e3cec6
Jorge Sanz <jorge@jorgesanz.net>
2022-11-24 12:51:27 +0100

    initial import

---
+++ src/index.js
@@ -0,0 +1,96 @@
+const http = require('http');
+const fs = require('fs');
+
+const { Client } = require('@elastic/elasticsearch');
+
+const port = process.env.PORT || 8080;
+
+const node = process.env.ELASTIC_HOST || 'http://localhost:9200';
+const username = process.env.ELASTIC_USER || 'elastic';
+const password = process.env.ELASTIC_PASSWORD || 'changeme';
+
+const index = process.env.ELASTIC_INDEX || 'flight_tracking*';
+const geomField = process.env.ELASTIC_GEOM_FIELD || 'location';
+
+const client = new Client({ node, auth: { username, password } });
+
+const server = http.createServer(async function (request, response) {
+    // Set CORS headers
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Request-Method', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+
+    if (request.url.startsWith('/tile')) {
+
+        // Template for /tile/{z}/{x}/{y}
+        const match = request.url.match(/tile\/(?<zoom>\d+)\/(?<x>\d+)\/(?<y>\d+)/);
+
+        if (!match) {
+            console.error(`[500] wrong tile request: ${request.url}`);
+            response.writeHead(500);
+            response.write('This does not seems a tile request\r\n');
+            response.end();
+            return;
+        }
+
+        try {
+            const { x, y, zoom } = match.groups;
+
+            const body = {
+                exact_bounds: true,
+                extent: 4096,
+                grid_agg: 'geohex',
+                grid_precision: 5,
+                grid_type: 'grid',
+                size: 0,
+                track_total_hits: false,
+                query: {
+                    "match_all": {}
+                }
+            }
+
+            const tile = await client.searchMvt({
+                index,
+                field: geomField,
+                zoom,
+                x,
+                y,
+                ...body,
+            }, { meta: true });
+
+            // set response header
+            response.writeHead(tile.statusCode, {
+                'content-disposition': 'inline',
+                'content-length': 'content-length' in tile.headers ? tile.headers['content-length'] : `0`,
+                'Content-Type': 'content-type' in tile.headers ? tile.headers['content-type'] : 'application/x-protobuf',
+                'Cache-Control': `public, max-age=0`,
+                'Last-Modified': `${new Date().toUTCString()}`,
+            });
+
+            // set response content
+            response.write(tile.body);
+            console.log(`[200] Sending tile ${zoom}/${x}/${y}`);
+        } catch (e) {
+            console.error(`[500] ${JSON.stringify(e)}`);
+            response.writeHead('statusCode' in e ? e.statusCode : 500);
+            response.write(e?.meta?.body ? JSON.stringify(e?.meta?.body) : '');
+            response.end();
+        }
+
+        response.end();
+    } else if (request.url === '/') {
+        console.error(`[200] Serving the frontpage`);
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(fs.readFileSync('./src/index.html'));
+        response.end('');
+    } else {
+        console.error(`[400] page does not exist: ${request.url}`);
+        response.writeHead(404);
+        response.write('Page does not exist')
+        response.end();
+    }
+});
+
+server.listen(port);
+console.log(`Tile server running on port ${port}`);
